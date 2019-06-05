744bb8c3464c4aa19eddf83bf8130649cd7394f4
Julien Marrec <julien.marrec@gmail.com>
2019-06-05 18:42:48 +0200

    Limit travis builds to GCC 7 & 8 on unix and add remote

--- .ci/install.sh
+++ .ci/install.sh
@@ -20,5 +20,6 @@ fi
 
 pip install conan --upgrade
 pip install conan_package_tools bincrafters_package_tools
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 
 conan user
