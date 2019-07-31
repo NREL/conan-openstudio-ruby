4a95a9f9f3cc0fb7a15898d8d90abafa618f0d69
Julien Marrec <julien.marrec@gmail.com>
2019-07-31 15:55:27 +0200

    Change CI settings to rely on Conan package tools add_common_builds

--- build.py
+++ build.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-from bincrafters import (build_template_default,
-                         build_template_installer,
-                         build_shared)
+from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
-    if "ARCH" in os.environ:
-        arch = os.environ["ARCH"]
-        builder = build_template_installer.get_builder(build_policy="outdated")
-        builder.add(settings={"os": build_shared.get_os(),
-                              "arch_build": arch,
-                              "arch": arch})
-    else:
-        builder = build_template_default.get_builder(build_policy="outdated")
-
-builder.run()
+    # This will add common builds.
+    # We are customizing what these are by using environment variables.
+    # Because we pass:
+    # * CONAN_ARCHS=x86_64, it will only produce the x64
+    # * CONAN_APPLE_CLANG_VERSIONS/CONAN_GCC_VERSIONS/CONAN_VISUAL_VERSIONS,
+    #   it will only use the specific one that's passed
+    # * CONAN_BUILD_TYPES, it will limit itself to the ones we asked
+    # cf: https://github.com/conan-io/conan-package-tools
+    # Specifically in ./cpt/builds_generator.py
+    builder = build_template_default.get_builder(build_policy="outdated")
+    builder.run()
