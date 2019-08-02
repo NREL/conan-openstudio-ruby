61406020248a9974393cc5c722495fd739e96d48
Julien Marrec <julien.marrec@gmail.com>
2019-08-02 15:48:03 +0200

    Use an environment variable pending https://github.com/bincrafters/bincrafters-package-tools/pull/25

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 from cpt.ci_manager import GenericManager
 from bincrafters import build_template_default
 
@@ -17,6 +18,11 @@ if __name__ == "__main__":
         # It can't be stable since that means it'd be 'master' => never upload
         upload_only_when_stable = True
 
+    # Pending https://github.com/bincrafters/bincrafters-package-tools/pull/25
+    # We can't use this upload_only_when_stable param so instead we set it as
+    # an env var
+    os.environ["CONAN_UPLOAD_ONLY_WHEN_STABLE"] = upload_only_when_stable
+
     # This will add common builds.
     # We are customizing what these are by using environment variables.
     # Because we pass:
@@ -28,5 +34,5 @@ if __name__ == "__main__":
     # Specifically in ./cpt/builds_generator.py
 
     builder = build_template_default.get_builder(build_policy="outdated",
-                                                 upload_only_when_stable=upload_only_when_stable)
+                                                 # upload_only_when_stable=upload_only_when_stable,
     builder.run()
