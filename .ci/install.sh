3cbffe1a22f62fca1ae1da5722c75ac8134b07bc
Julien Marrec <julien.marrec@gmail.com>
2019-08-12 19:05:27 +0200

    Force install libdgbm-dev on linux...

--- .ci/install.sh
+++ .ci/install.sh
@@ -2,22 +2,31 @@
 
 set -ex
 
-if [[ "$(uname -s)" == 'Darwin' ]]; then
-    brew update || brew update
-    brew outdated pyenv || brew upgrade pyenv
-    brew install pyenv-virtualenv
-    brew install cmake || true
-
-    if which pyenv > /dev/null; then
-        eval "$(pyenv init -)"
-    fi
-
-    pyenv install 3.7.1
-    pyenv virtualenv 3.7.1 conan
-    pyenv rehash
-    pyenv activate conan
+unameOut="$(uname -s)"
+case "${unameOut}" in
+  Linux*)     machine=Linux;;
+  Darwin*)    machine=Darwin;;
+  CYGWIN*|MINGW*|MSYS*)    machine=Windows;;
+  *)          machine="UNKNOWN:${unameOut}"
+esac
 
 
+if [[ $machine == Darwin ]]; then
+  brew update || brew update
+  brew outdated pyenv || brew upgrade pyenv
+  brew install pyenv-virtualenv
+  brew install cmake || true
+
+  if which pyenv > /dev/null; then
+    eval "$(pyenv init -)"
+  fi
+
+  pyenv install 3.7.1
+  pyenv virtualenv 3.7.1 conan
+  pyenv rehash
+  pyenv activate conan
+elif [[ $machine == Linux ]]; then
+  apt install libgdbm-dev
 fi
 
 pip install conan --upgrade
