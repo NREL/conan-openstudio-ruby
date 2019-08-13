eebc860a76b5af0093aad5e0b556017c9f67fced
Julien Marrec <julien.marrec@gmail.com>
2019-08-13 17:00:15 +0200

    Some house cleaning in test, very rough

--- test_package/test/bundle_git/test.rb
+++ test_package/test/bundle_git/test.rb
@@ -5,24 +5,11 @@ def local_gems
 end
 
 # list installed gems
-puts local_gems.map{ |name, specs| 
-  [name, specs.map{ |spec| spec.version.to_s }.join(',')].join(' ') 
+puts local_gems.map{ |name, specs|
+  [name, specs.map{ |spec| spec.version.to_s }.join(',')].join(' ')
 }
 
 # test a github checkout gem
-require 'openstudio'
-require 'openstudio/extension'
-puts OpenStudio::Extension::VERSION
-raise "OpenStudio Extension version does not match" unless OpenStudio::Extension::VERSION.to_s == '0.1.1'
-
 require 'tilt'
 puts Tilt::VERSION
 raise "Tilt version does not match" unless Tilt::VERSION == '2.0.8'
-
-did_fail = false
-begin
-  require 'openstudio-standards'
-rescue LoadError
-  did_fail = true
-end
-raise "Should not load openstudio-standards" unless did_fail == true
--- test_package/test/bundle_no_install/test.rb
+++ test_package/test/bundle_no_install/test.rb
@@ -5,17 +5,11 @@ def local_gems
 end
 
 # list installed gems
-puts local_gems.map{ |name, specs| 
-  [name, specs.map{ |spec| spec.version.to_s }.join(',')].join(' ') 
+puts local_gems.map{ |name, specs|
+  [name, specs.map{ |spec| spec.version.to_s }.join(',')].join(' ')
 }
 
 # test a simple gem
 require 'tilt'
 puts Tilt::VERSION
 raise "Tilt version does not match" unless Tilt::VERSION == '2.0.8'
-
-# test a more complex / larger gem, that should override the existing version installed
-require 'openstudio'
-require 'openstudio-standards'
-puts OpenstudioStandards::VERSION
-raise "OpenStudio Standards version does not match" unless OpenstudioStandards::VERSION == '0.2.0'
