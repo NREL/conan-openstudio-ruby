f809186365515cad7d1e339b5fd40bda92620b5b
Julien Marrec <julien.marrec@gmail.com>
2019-08-14 14:06:06 +0200

    Improve reporting of tests etc

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -54,30 +54,40 @@ class TestPackageConan(ConanFile):
         return all_test_args
 
     def test(self):
-        cli_path = os.path.abspath("openstudio")
+        def _report_test_results(test_args, success=False):
+            n = len(test_args)
+            status = "Passed" if success else "Failed"
+            msg = "{} tests {}:".format(n, status)
+            for test_arg in test_args:
+                rel_path = os.path.relpath(test_arg[0],
+                                           start=self.source_folder)
+                msg += "\n * {}: {}".format(rel_path, test_arg[1])
+            return msg
+
+        cli_path = os.path.abspath(os.path.join("bin", "openstudio"))
         print("CWD={}".format(os.path.abspath('.')))
         print("source_folder={}".format(os.path.abspath(self.source_folder)))
 
         # No point in trying to run tests if the --help doesn't even work
-        self.output.info("Running openstudio --help")
         self.run('{} --help'.format(cli_path))
+        self.output.success("Test Passed - Running openstudio --help")
 
         all_test_args = self._discover_tests()
         failed_tests = []
+        passed_tests = []
         for test_arg in all_test_args:
             cmd = "{} {} --name={}".format(cli_path, test_arg[0], test_arg[1])
             self.output.info(cmd)
             # Run all tests even if failed, we'll unwind later
             try:
                 self.run(cmd)
+                passed_tests.append(test_arg)
+
             except ConanException:
                 failed_tests.append(test_arg)
 
-        if failed_tests:
-            msg = "{} tests failed:".format(len(failed_tests))
-            for failed_test in failed_tests:
-                rel_path = os.path.relpath(failed_test[0],
-                                           start=self.source_folder)
-                msg += "\n * {}: {}".format(rel_path, failed_test[1])
+        self.output.success(_report_test_results(passed_tests, success=True))
 
-            raise TestFailedException(msg)
+        if failed_tests:
+            raise TestFailedException(_report_test_results(failed_tests,
+                                                           success=False))
