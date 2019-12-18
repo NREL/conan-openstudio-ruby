e41c3380bc76a4ceb596693cec43e68805a62062
Julien Marrec <julien.marrec@gmail.com>
2019-12-18 11:00:45 +0100

    Try to use short_paths to avoid 260 char problem when testing package on AppVeyor [skip travis]

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -13,6 +13,10 @@ class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
+    # Windows only: avoid hitting the 260 character limit
+    # https://docs.conan.io/en/latest/reference/conanfile/attributes.html#short-paths
+    short_paths = True
+
     def requirements(self):
         """
         Declare required dependencies for testing
