3c9971e2513ca31ad522811171e1b39051ad5212
Julien Marrec <julien.marrec@gmail.com>
2020-07-24 17:02:24 +0200

    Mac exceeded build job length... try again

--- .ci/run.sh
+++ .ci/run.sh
@@ -13,48 +13,50 @@ fi
 
 python build.py
 
-if [[ "$(uname -s)" == 'Darwin' ]]; then
-  find ~/.conan/data/openstudio_ruby/2.5.5/nrel/testing/build -name "mkmf.log"|while read fname; do
-    echo "============== $fname ============"
-    cat $fname
-    echo ""
-    echo ""
-    echo ""
-  done
+# Debug: how to retrieve logs: remove the "-e" option above, then cat the logs you want:
 
-  echo "======== LISTING THE GDBM LIB FOLDERS ========"
-  find ~/.conan/data/gdbm/1.18.1/_/_/package/ -name 'lib' -type 'd'|while read dirname; do
-    echo $dirname
-    ls $dirname
-    echo ""
-    echo ""
-    echo ""
-  done
+#if [[ "$(uname -s)" == 'Darwin' ]]; then
+  #find ~/.conan/data/openstudio_ruby/2.5.5/nrel/testing/build -name "mkmf.log"|while read fname; do
+    #echo "============== $fname ============"
+    #cat $fname
+    #echo ""
+    #echo ""
+    #echo ""
+  #done
 
-  echo "======== LISTING THE GDBM INCLUDE FOLDERS ========"
-  find ~/.conan/data/gdbm/1.18.1/_/_/package/ -name 'include' -type 'd'|while read dirname; do
-    echo $dirname
-    ls $dirname
-    echo ""
-    echo ""
-    echo ""
-  done
+  #echo "======== LISTING THE GDBM LIB FOLDERS ========"
+  #find ~/.conan/data/gdbm/1.18.1/_/_/package/ -name 'lib' -type 'd'|while read dirname; do
+    #echo $dirname
+    #ls $dirname
+    #echo ""
+    #echo ""
+    #echo ""
+  #done
 
-  echo "=========== RUBY CONFIG.LOG ================"
-  find ~/.conan/data/openstudio_ruby/2.5.5/nrel/testing/build -name "config.log"|while read fname; do
-    echo "============== $fname ============"
-    cat $fname
-    echo ""
-    echo ""
-    echo ""
-  done
+  #echo "======== LISTING THE GDBM INCLUDE FOLDERS ========"
+  #find ~/.conan/data/gdbm/1.18.1/_/_/package/ -name 'include' -type 'd'|while read dirname; do
+    #echo $dirname
+    #ls $dirname
+    #echo ""
+    #echo ""
+    #echo ""
+  #done
 
-  echo "=========== RUBY CONFIG.STATUS ================"
-  find ~/.conan/data/openstudio_ruby/2.5.5/nrel/testing/build -name "config.status"|while read fname; do
-    echo "============== $fname ============"
-    cat $fname
-    echo ""
-    echo ""
-    echo ""
-  done
-fi
+  #echo "=========== RUBY CONFIG.LOG ================"
+  #find ~/.conan/data/openstudio_ruby/2.5.5/nrel/testing/build -name "config.log"|while read fname; do
+    #echo "============== $fname ============"
+    #cat $fname
+    #echo ""
+    #echo ""
+    #echo ""
+  #done
+
+  #echo "=========== RUBY CONFIG.STATUS ================"
+  #find ~/.conan/data/openstudio_ruby/2.5.5/nrel/testing/build -name "config.status"|while read fname; do
+    #echo "============== $fname ============"
+    #cat $fname
+    #echo ""
+    #echo ""
+    #echo ""
+  #done
+#fi
