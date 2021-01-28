3fd4eab02960da1c99ac503e0de59e8ce9294c4e
Julien Marrec <julien.marrec@gmail.com>
2021-01-28 14:31:57 +0100

    Use bundled ffi, adjust configure call, use openssl 1.1.1i, and enable readline. Per the github workflow I found
    https://github.com/ruby/ruby/blob/master/.github/workflows/windows.yml

--- conanfile.py
+++ conanfile.py
@@ -38,7 +38,8 @@ class OpenstudiorubyConan(ConanFile):
             self.output.warn(
                 "Readline (hence GDBM) will not work on MSVC right now")
             self.options.with_gdbm = False
-            self.options.with_readline = False
+            # TODO: temporary try
+            self.options.with_readline = True
             self.output.warn(
                 "Conan LIBFFI will not allow linking right now with MSVC, "
                 "so temporarilly built it from CMakeLists instead")
@@ -74,7 +75,7 @@ class OpenstudiorubyConan(ConanFile):
         """
         Declare required dependencies
         """
-        self.requires("openssl/1.1.0l") # fails with 1.1.1h https://github.com/openssl/openssl/issues/3884
+        self.requires("openssl/1.1.1i") # fails with 1.1.1h https://github.com/openssl/openssl/issues/3884
         self.requires("zlib/1.2.11")
 
         if self.options.with_libyaml:
